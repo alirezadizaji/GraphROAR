3f73b2dd22d2c8d99a87a17c9d95e2356dcd17e1
AlirezaDizaji <alirezadizaji@yahoo.com>
2022-07-29 19:18:55 +0430

    Fix

--- src/GNN_Explainability/entrypoints/explain_gcn/ba2motifs/pg_explainer.py
+++ src/GNN_Explainability/entrypoints/explain_gcn/ba2motifs/pg_explainer.py
@@ -5,31 +5,42 @@ from dig.xgraph.method import PGExplainer
 from dig.xgraph.models import *
 import numpy as np
 import torch
-from torch_geometric.data import DataLoader
-from torch_geometric.data import DataLoader, Data
+from torch_geometric.data import Data
+from torch_geometric.utils import remove_self_loops
 
-from ....data.ba_2motifs import BA2MotifsDataset
-from ....enums.data_spec import DataSpec
+from GNN_Explainability.config.base_config import BaseConfig
+
+from ....enums import Dataset
 from ...main import MainEntrypoint
 
 class Entrypoint(MainEntrypoint):
-    def run():
-        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-        
-        train_loader = DataLoader(BA2MotifsDataset(DataSpec.TRAIN), shuffle=False)
-        val_loader = DataLoader(BA2MotifsDataset(DataSpec.VAL), shuffle=False)
-        test_loader = DataLoader(BA2MotifsDataset(DataSpec.TEST), shuffle=False)
-        
-        model = GIN_3l(model_level='graph', dim_node=10, dim_hidden=300, num_classes=2)
-        model.to(device)
-        model.load_state_dict(torch.load('./GNN_Explainability/checkpoints/ba2motifs_gin_3l.pt', map_location=device))
-        
-        explainer = PGExplainer(model, in_channels=600, device=device, explain_graph=True, epochs=100, k=3)
+    def __init__(self):
+        conf = BaseConfig(
+            try_num=3,
+            try_name='pgexplainer',
+            dataset_name=Dataset.BA2Motifs,
+        )
+        conf.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        conf.num_epochs = 10
+        conf.save_log_in_file = False
 
-        for dataspec, loader in zip(['train', 'val', 'test'], [train_loader, val_loader, test_loader]):
+        conf.base_model = GIN_3l(model_level='graph', dim_node=10, dim_hidden=300, num_classes=2)
+        conf.base_model.load_state_dict(torch.load('./GNN_Explainability/checkpoints/ba2motifs_gin_3l.pt', map_location=conf.device))
+        self.roar_percentage: float = None
+
+        super(Entrypoint, self).__init__(conf)
+
+    def run(self):
+        
+        explainer = PGExplainer(self.conf.base_model, in_channels=600, device=self.conf.device, explain_graph=True, epochs=100, k=1)
+
+        for dataspec, loader in zip(
+                ['train', 'val', 'test'],
+                [self.conf.train_loader, self.conf.val_loader, self.conf.test_loader]):
             dataset = []
             for i, data in enumerate(loader):
                 data: Data = data[0]
+                data.edge_index = remove_self_loops(data.edge_index)[0]
                 dataset.append(data)
 
             explainer.train_explanation_network(dataset)
@@ -56,7 +67,7 @@ class Entrypoint(MainEntrypoint):
                 if torch.any(edge_mask_new == -100):
                     raise Exception('there is an unhandled edge mask')
                 
-                edge_mask_new = edge_mask_new.cpu().numpy()
+                edge_mask_new = edge_mask_new.detach().cpu().numpy()
                 file_dir = os.path.join('GNN_Explainability', 'data', 'ba2motifs', 'gnnexplainer', dataspec)
                 os.makedirs(file_dir, exist_ok=True)
                 
--- src/GNN_Explainability/utils/visualization.py
+++ src/GNN_Explainability/utils/visualization.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import networkx as nx
+from torch_geometric.data import Data
+from torch_geometric.utils import to_networkx
 
-def visualization(graph, title: str):
+def visualization(data: Data, title: str):
+    graph = to_networkx(data)
     pos = nx.kamada_kawai_layout(graph)
-
     nx.draw_networkx_nodes(graph, pos,
                         node_size=300)
 
@@ -12,6 +14,7 @@ def visualization(graph, title: str):
     nx.draw_networkx_edges(graph, pos,
         width=6,
         arrows=False)
+    nx.draw_networkx_labels(graph, pos)
     
     plt.title(title)
     plt.show()
