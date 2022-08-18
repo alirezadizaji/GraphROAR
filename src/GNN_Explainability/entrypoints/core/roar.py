8b89997e34c7ca236408736e01fcf63e76267937
AlirezaDizaji <alirezadizaji@yahoo.com>
2022-08-18 12:57:20 +0430

    Report results for best epoch

--- src/GNN_Explainability/entrypoints/core/roar.py
+++ src/GNN_Explainability/entrypoints/core/roar.py
@@ -16,21 +16,28 @@ if TYPE_CHECKING:
 class ROAREntrypoint(TrainEntrypoint):
     def __init__(self, conf: 'ROARConfig', model: 'GNNBasic') -> None:
         model = GNNWrapper(model)
+        self.roar_ratio: float = None
         super().__init__(conf, model)
 
     def _model_forwarding(self, data: 'Batch') -> torch.Tensor:
         x = self.model([data])
         return x
     
-    def _save_model_weight(self, name: str='model') -> None:
-        wrapper: GNNWrapper = self.model
-        torch.save(wrapper.model.state_dict(), os.path.join(self.conf.save_dir, f'{name}.pt'))
+    def _get_weight_save_path(self, epoch_num):
+        weight_save_path = super()._get_weight_save_path(epoch_num)
+        dirname = os.path.dirname(weight_save_path)
+        filename = os.path.basename(weight_save_path)
+        new_dirname = os.path.join(dirname, str(self.roar_ratio))
+        os.makedirs(new_dirname, exist_ok=True)
+
+        weight_save_path = os.path.join(new_dirname, filename)
+        return weight_save_path
 
     @contextmanager
-    def eliminate_edges(self, roar_ratio: float):
+    def eliminate_edges(self):
         conf: 'ROARConfig' = self.conf
         handle = self.model.register_forward_pre_hook(
-                edge_elimination_hook(Arguments(conf.edge_masks_load_dir, roar_ratio, conf.edge_mask_symmetric)))
+                edge_elimination_hook(Arguments(conf.edge_masks_load_dir, self.roar_ratio, conf.edge_mask_symmetric)))
         
         yield None
 
@@ -39,9 +46,8 @@ class ROAREntrypoint(TrainEntrypoint):
     def run(self):
         conf: 'ROARConfig' = self.conf
 
-        for ratio in conf.roar_ratios:
-            print(f"*** ROAR {ratio*100}% ***", flush=True)
-            with self.eliminate_edges(ratio):
+        for self.roar_ratio in conf.roar_ratios:
+            print(f"*** ROAR {self.roar_ratio*100}% ***", flush=True)
+            with self.eliminate_edges():
                 super().run()
-            self._save_model_weight(f'model_roar{ratio*100}%')
 
--- src/GNN_Explainability/entrypoints/core/train.py
+++ src/GNN_Explainability/entrypoints/core/train.py
@@ -1,3 +1,4 @@
+import math
 import os
 from tqdm import tqdm
 from typing import TYPE_CHECKING
@@ -7,6 +8,8 @@ import numpy as np
 import torch
 from torch.nn import functional as F
 
+from ...enums.data_spec import DataSpec
+
 from . import MainEntrypoint
 
 if TYPE_CHECKING:
@@ -15,7 +18,14 @@ if TYPE_CHECKING:
 
 
 class TrainEntrypoint(MainEntrypoint):
-    
+
+    def _get_weight_save_path(self, epoch_num):
+        weight_dir_name = 'weights'
+        weight_save_dir =  os.path.join(self.conf.save_dir, weight_dir_name)
+        os.makedirs(weight_save_dir, exist_ok=True)
+        weight_save_path = os.path.join(weight_save_dir, str(epoch_num))
+        return weight_save_path
+
     def _model_forwarding(self, data: 'Batch') -> torch.Tensor:
         x = self.model(data=data)
         return x
@@ -26,8 +36,8 @@ class TrainEntrypoint(MainEntrypoint):
         loss = - torch.mean(x[torch.arange(x.size(0)), y])
         return loss
 
-    def _save_model_weight(self, name: str='model') -> None:
-        torch.save(self.model.state_dict(), os.path.join(self.conf.save_dir, f'{name}.pt'))
+    def _save_model_weight(self, epoch_num: int) -> None:
+        torch.save(self.model.state_dict(), self._get_weight_save_path(epoch_num))
 
     def _train_for_one_epoch(self, epoch_num: int) -> None:
         conf: 'BaseConfig' = self.conf
@@ -57,7 +67,6 @@ class TrainEntrypoint(MainEntrypoint):
     
         print(f'epoch {epoch_num} train acc {correct/total}', flush=True)
 
-
     def _validate_for_one_epoch(self, epoch_num: int, loader: 'DataLoader') -> None:
         self.model.eval()
 
@@ -71,14 +80,22 @@ class TrainEntrypoint(MainEntrypoint):
                 correct += torch.sum(y_pred == data.y).item()
                 total += data.y.numel()
             
-            print(f'epoch{epoch_num} {loader.dataset.dataspec.lower()} acc {correct/total}', flush=True)
-
+            acc = correct / total
+            print(f'epoch{epoch_num} {loader.dataset.dataspec.lower()} acc {acc}', flush=True)
+            return acc
 
     def run(self):
+        val_accs = np.zeros(self.conf.training_config.num_epochs)
+
         for epoch in tqdm(range(self.conf.training_config.num_epochs)):
             self.epoch = epoch
             self._train_for_one_epoch(epoch)  
-            self._validate_for_one_epoch(epoch, self.val_loader)              
+            self._save_model_weight(epoch)
+            val_accs[epoch] = self._validate_for_one_epoch(epoch, self.val_loader)              
         
-        self._validate_for_one_epoch(epoch, self.test_loader)
-        self._save_model_weight()
+        # evaluate best epoch on test set
+        best_epoch = val_accs.argmax()
+        self.model.load_state_dict(torch.load(self._get_weight_save_path(best_epoch), map_location=self.conf.device))
+        test_acc =self._validate_for_one_epoch(epoch, self.test_loader)
+        val_acc = val_accs[best_epoch]
+        print(f"@@@@@ Best accuracy epoch {best_epoch} @@@@@\nval: {val_acc}, test: {test_acc}", flush=True)
