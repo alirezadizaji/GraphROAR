9ea3cc0093d23872c10e2c88c6501d9d2903f14a
AlirezaDizaji <alirezadizaji@yahoo.com>
2022-08-22 15:09:59 +0430

    print

--- src/GNN_Explainability/utils/seed_changer.py
+++ src/GNN_Explainability/utils/seed_changer.py
@@ -7,7 +7,7 @@ from main import global_seed
 @contextmanager
 def seed_changer(end=100000):
     seed = int(time.time()) % end
-
+    print(f"@@@@@ seed changes to {seed} @@@@@", flush=True)
     global_seed(seed)        
     yield
     # BUG: reverting seed is hard code
