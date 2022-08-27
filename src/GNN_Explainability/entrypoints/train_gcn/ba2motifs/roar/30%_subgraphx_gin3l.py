3acfa77fdacbe8788d6cc61b89f257cb186a0e1d
AlirezaDizaji <alirezadizaji@yahoo.com>
2022-08-27 10:55:44 +0430

    Fix

--- src/GNN_Explainability/entrypoints/train_gcn/ba2motifs/roar/30%_subgraphx_gin3l.py
+++ src/GNN_Explainability/entrypoints/train_gcn/ba2motifs/roar/30%_subgraphx_gin3l.py
@@ -17,7 +17,7 @@ class Entrypoint(ROAREntrypoint):
     def __init__(self):
         conf = ROARConfig(
             try_num=32,
-            try_name='roar_subgraphx_0.1_gin3l',
+            try_name='roar_subgraphx_0.3_gin3l',
             dataset_name=Dataset.BA2Motifs,
             training_config=TrainingConfig(100, OptimType.ADAM),
             device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu'),
--- src/GNN_Explainability/entrypoints/train_gcn/ba2motifs/roar/90%_subgraphx_gin3l.py
+++ src/GNN_Explainability/entrypoints/train_gcn/ba2motifs/roar/90%_subgraphx_gin3l.py
@@ -17,7 +17,7 @@ class Entrypoint(ROAREntrypoint):
     def __init__(self):
         conf = ROARConfig(
             try_num=35,
-            try_name='roar_subgraphx_0.7_gin3l',
+            try_name='roar_subgraphx_0.9_gin3l',
             dataset_name=Dataset.BA2Motifs,
             training_config=TrainingConfig(100, OptimType.ADAM),
             device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu'),
