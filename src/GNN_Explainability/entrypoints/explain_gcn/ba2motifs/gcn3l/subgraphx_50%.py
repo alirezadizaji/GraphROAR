5511416a2f3f41c6151f492e45595436fa82be3a
AlirezaDizaji <alirezadizaji@yahoo.com>
2022-08-29 15:30:19 +0430

    Feature: add `SubgraphX` experiments for `GCN_3l` on `BA2Motifs`

---
+++ src/GNN_Explainability/entrypoints/explain_gcn/ba2motifs/gcn3l/subgraphx_50%.py
@@ -0,0 +1,42 @@
+import os
+
+from dig.xgraph.method import SubgraphX
+from dig.xgraph.models import *
+import torch
+
+
+from .....config import SubgraphXConfig, TrainingConfig
+from .....entrypoints.core.explanation.subgraphx import SubgraphXEntrypoint
+
+from .....enums import *
+
+
+class Entrypoint(SubgraphXEntrypoint):
+    def __init__(self):
+        conf = SubgraphXConfig(
+            try_num=50,
+            try_name='subgraphx_gcn3l',
+            dataset_name=Dataset.BA2Motifs,
+            training_config=TrainingConfig(100, OptimType.ADAM, batch_size=1),
+            device=torch.device('cuda:1' if torch.cuda.is_available() else 'cpu'),
+            save_log_in_file=True,
+            num_classes=2,
+            save_visualization=True,
+            visualize_explainer_perf=True,
+            num_instances_to_visualize=20,
+            edge_mask_save_dir=os.path.join('..', 'data', 'ba_2motifs', 'explanation', 'gcn3l', 'subgraphx_50%'),
+            sparsity=0.0,
+            node_color_setter=None,
+            plt_legend=None,
+            explain_graph=True,
+            reward_method='mc_shapley',
+            get_max_nodes=(lambda data: int(data.edge_index.size(1)/2 * 0.5) + 1),
+            n_rollout=10
+        )
+
+        model = GCN_3l_BN(model_level='graph', dim_node=10, dim_hidden=20, num_classes=2)
+        model.load_state_dict(torch.load('../results/1_gcn3l_BA2Motifs/weights/16', map_location=conf.device))
+
+        # explainer will be initiated during explaining an instance
+        explainer = None
+        super().__init__(conf, model, explainer)
