d59ca9e27235eeafb5c7a8f8e1ff62e378cca7ad
AlirezaDizaji <alirezadizaji@yahoo.com>
2022-08-29 13:51:07 +0430

    Feature: add gcn3l explanations

--- src/GNN_Explainability/entrypoints/explain_gcn/ba2motifs/gcn3l/gnn_explainer.py
+++ src/GNN_Explainability/entrypoints/explain_gcn/ba2motifs/gcn3l/gnn_explainer.py
@@ -11,24 +11,24 @@ import torch
 
 from GNN_Explainability.config.base_config import TrainingConfig
 
-from ....entrypoints.core import GNNExplainerEntrypoint
+from .....entrypoints.core import GNNExplainerEntrypoint
 
-from ....config import GNNExplainerConfig
-from ....enums import *
+from .....config import GNNExplainerConfig
+from .....enums import *
 
 
 class Entrypoint(GNNExplainerEntrypoint):
     def __init__(self):
         conf = GNNExplainerConfig(
-            try_num=2,
-            try_name='gnnexplainer',
+            try_num=36,
+            try_name='gnnexplainer_gcn3l',
             dataset_name=Dataset.BA2Motifs,
             training_config=TrainingConfig(300, OptimType.ADAM, 0.01, batch_size=1),
             save_log_in_file=True,
             num_classes=2,
             save_visualization=True,
             visualize_explainer_perf=True,
-            edge_mask_save_dir=os.path.join('..', 'data', 'ba_2motifs', 'explanation', 'gnnexplainer'),
+            edge_mask_save_dir=os.path.join('..', 'data', 'ba_2motifs', 'explanation', 'gcn3l', 'gnnexplainer'),
             num_instances_to_visualize=10,
             explain_graph=True,
             mask_features=True,
@@ -39,9 +39,9 @@ class Entrypoint(GNNExplainerEntrypoint):
             device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu'),
             coff_node_feat_size=0.0)
         
-        model = GIN_3l(model_level='graph', dim_node=10, dim_hidden=300, num_classes=conf.num_classes)
+        model = GCN_3l_BN(model_level='graph', dim_node=10, dim_hidden=20, num_classes=conf.num_classes)
         model.to(conf.device)
-        model.load_state_dict(torch.load('../checkpoints/ba2motifs_gin_3l.pt', map_location=conf.device))
+        model.load_state_dict(torch.load('../1_gcn3l_BA2Motifs/weights/16', map_location=conf.device))
         
         explainer = GNNExplainer(model, epochs=conf.training_config.num_epochs,
                  lr=conf.training_config.lr, explain_graph=conf.explain_graph, 
--- src/GNN_Explainability/entrypoints/explain_gcn/ba2motifs/pg_explainer.py
+++ src/GNN_Explainability/entrypoints/explain_gcn/ba2motifs/pg_explainer.py
@@ -3,6 +3,7 @@ import os
 
 from dig.xgraph.method import PGExplainer
 from dig.xgraph.models import *
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torch_geometric.data import Data
@@ -27,14 +28,14 @@ class Entrypoint(MainEntrypoint):
         conf.device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
         conf.save_log_in_file = False
 
-        model = GIN_3l(model_level='graph', dim_node=10, dim_hidden=300, num_classes=2)
-        model.load_state_dict(torch.load('../checkpoints/ba2motifs_gin3l.pt', map_location=conf.device))
+        model = GCN_3l_BN(model_level='graph', dim_node=10, dim_hidden=20, num_classes=2)
+        model.load_state_dict(torch.load('../results/1_gcn3l_BA2Motifs/weights/14', map_location=conf.device))
 
         super(Entrypoint, self).__init__(conf, model)
 
     def run(self):
         
-        explainer = PGExplainer(self.model, in_channels=600, 
+        explainer = PGExplainer(self.model, in_channels=40, 
                 device=self.conf.device, explain_graph=True,
                 epochs=self.conf.training_config.num_epochs, 
                 lr=3e-3, coff_size=0.0, coff_ent=0.0, t0=5.0, t1=1.0, sample_bias=0.0)
@@ -43,31 +44,35 @@ class Entrypoint(MainEntrypoint):
         for i, data in enumerate(self.train_loader):
             data: Data = data[0]
             data.edge_index = remove_self_loops(data.edge_index)[0]
-            data.edge_index = add_remaining_self_loops(data.edge_index)[0]
+            # data.edge_index = add_remaining_self_loops(data.edge_index)[0]
             dataset.append(data)
 
         explainer.train_explanation_network(dataset)
 
-        torch.save(explainer.state_dict(), '../checkpoints/ba2motifs_gin_3l_pgexplainer.pt')
-        # explainer.load_state_dict(torch.load('./GNN_Explainability/checkpoints/ba2motifs_gin_3l_pgexplainer.pt', map_location=self.conf.device))
+        # torch.save(explainer.state_dict(), '../checkpoints/ba2motifs_gin_3l_pgexplainer.pt')
+        # explainer.load_state_dict(torch.load('../checkpoints/ba2motifs_gin_3l_pgexplainer.pt', map_location=self.conf.device))
 
         for loader in [self.train_loader, self.val_loader, self.test_loader]:
             for i, data in enumerate(loader):
                 data: Data = data[0]
                 data.edge_index = remove_self_loops(data.edge_index)[0]
-                data.edge_index = add_remaining_self_loops(data.edge_index)[0]
+                # data.edge_index = add_remaining_self_loops(data.edge_index)[0]
                 
                 print(f'explain graph {i} gt label {data.y}', flush=True)
                 _, masks, _ = \
                         explainer(data.x, data.edge_index, y=data.y)
                 
                 pos = visualization(data, data.y.item())
+                plt.close()
                 edge_mask = masks[0]
-                print(edge_mask)
-                data.edge_index = data.edge_index[:, edge_mask >= 0.5]
-                visualization(data, data.y.item(), pos)
-                file_dir = os.path.join('GNN_Explainability', 'data', 'ba_2motifs', 'explanation', 'pgexplainer')
-                os.makedirs(file_dir, exist_ok=True)
+                print(edge_mask.topk(14)[1])
+                data.edge_index = data.edge_index[:, edge_mask.topk(10)[1]]
+                visualization(data, f"{data.name[0]}_{data.y.item()}", pos, 
+                # save_dir='../results/1_pgggg'
+                )
+                plt.close()
+                # file_dir = os.path.join('GNN_Explainability', 'data', 'ba_2motifs', 'explanation', 'pgexplainer')
+                # os.makedirs(file_dir, exist_ok=True)
                 
-                file_name = f"{i}"
-                np.save(os.path.join(file_dir, file_name), edge_mask.detach().cpu().numpy())                
+                # file_name = f"{i}"
+                # np.save(os.path.join(file_dir, file_name), edge_mask.detach().cpu().numpy())                
