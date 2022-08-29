6d366740c1457146af010d09ac291c6e3baf7b79
AlirezaDizaji <alirezadizaji@yahoo.com>
2022-08-29 14:38:51 +0430

    add PGExplainer experiments on GIN3l

--- src/GNN_Explainability/entrypoints/explain_gcn/ba2motifs/pg_explainer.py
+++ src/GNN_Explainability/entrypoints/explain_gcn/ba2motifs/pg_explainer.py
@@ -19,23 +19,22 @@ class Entrypoint(MainEntrypoint):
     def __init__(self):
         conf = BaseConfig(
             try_num=4,
-            try_name='ba2motifs_pgexplainer',
+            try_name='pgexplainer_gin3l',
             dataset_name=Dataset.BA2Motifs,
             device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu'),
-            save_log_in_file=False,
-            training_config=TrainingConfig(10, OptimType.ADAM, batch_size=1),
+            save_log_in_file=True,
+            training_config=TrainingConfig(30, OptimType.ADAM, batch_size=1),
         )
         conf.device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
-        conf.save_log_in_file = False
 
-        model = GCN_3l_BN(model_level='graph', dim_node=10, dim_hidden=20, num_classes=2)
-        model.load_state_dict(torch.load('../results/1_gcn3l_BA2Motifs/weights/14', map_location=conf.device))
+        model = GIN_3l(model_level='graph', dim_node=10, dim_hidden=300, num_classes=2)
+        model.load_state_dict(torch.load('../checkpoints/ba2motifs_gin_3l.pt', map_location=conf.device))
 
         super(Entrypoint, self).__init__(conf, model)
 
     def run(self):
         
-        explainer = PGExplainer(self.model, in_channels=40, 
+        explainer = PGExplainer(self.model, in_channels=600, 
                 device=self.conf.device, explain_graph=True,
                 epochs=self.conf.training_config.num_epochs, 
                 lr=3e-3, coff_size=0.0, coff_ent=0.0, t0=5.0, t1=1.0, sample_bias=0.0)
@@ -44,35 +43,46 @@ class Entrypoint(MainEntrypoint):
         for i, data in enumerate(self.train_loader):
             data: Data = data[0]
             data.edge_index = remove_self_loops(data.edge_index)[0]
-            # data.edge_index = add_remaining_self_loops(data.edge_index)[0]
             dataset.append(data)
 
         explainer.train_explanation_network(dataset)
 
-        # torch.save(explainer.state_dict(), '../checkpoints/ba2motifs_gin_3l_pgexplainer.pt')
-        # explainer.load_state_dict(torch.load('../checkpoints/ba2motifs_gin_3l_pgexplainer.pt', map_location=self.conf.device))
 
+        torch.save(explainer.state_dict(), '../results/4_pgexplainer_gin3l_BA2Motifs/weights/model.pt')
+        # explainer.load_state_dict(torch.load('../results/4_pgexplainer_gin3l_BA2Motifs/weights/model.pt', map_location=self.conf.device))
+
+        i_total = 0
         for loader in [self.train_loader, self.val_loader, self.test_loader]:
             for i, data in enumerate(loader):
+                i_total += 1
                 data: Data = data[0]
-                data.edge_index = remove_self_loops(data.edge_index)[0]
-                # data.edge_index = add_remaining_self_loops(data.edge_index)[0]
+                
+                edge_index = data.edge_index
+                mask = edge_index[0] != edge_index[1]
+                mask = mask.cpu().numpy()
                 
                 print(f'explain graph {i} gt label {data.y}', flush=True)
                 _, masks, _ = \
-                        explainer(data.x, data.edge_index, y=data.y)
+                        explainer(data.x, edge_index[:, mask], y=data.y)
                 
-                pos = visualization(data, data.y.item())
-                plt.close()
                 edge_mask = masks[0]
-                print(edge_mask.topk(14)[1])
-                data.edge_index = data.edge_index[:, edge_mask.topk(10)[1]]
-                visualization(data, f"{data.name[0]}_{data.y.item()}", pos, 
-                # save_dir='../results/1_pgggg'
-                )
-                plt.close()
-                # file_dir = os.path.join('GNN_Explainability', 'data', 'ba_2motifs', 'explanation', 'pgexplainer')
-                # os.makedirs(file_dir, exist_ok=True)
+
+                if i_total <= 20:
+                    pos = visualization(data, f"{data.name[0]}_{data.y.item()}_org", 
+                    save_dir='../results/4_pgexplainer_gin3l_BA2Motifs/visualizations')
+                    plt.close()
+
+                    
+                    data.edge_index = data.edge_index[:, edge_mask.topk(10)[1]]
+                    visualization(data, f"{data.name[0]}_{data.y.item()}_X", pos, 
+                    save_dir='../results/4_pgexplainer_gin3l_BA2Motifs/visualizations')
+                    plt.close()
                 
-                # file_name = f"{i}"
-                # np.save(os.path.join(file_dir, file_name), edge_mask.detach().cpu().numpy())                
+                new_edge_mask = np.ones_like(mask, dtype=np.float)
+                new_edge_mask[mask] = edge_mask.detach().cpu().numpy()
+                
+                file_dir = os.path.join('..', 'data', 'ba_2motifs', 'explanation', 'pgexplainer')
+                os.makedirs(file_dir, exist_ok=True)
+                
+                file_name = f"{data.name[0]}"
+                np.save(os.path.join(file_dir, file_name), new_edge_mask)                
---
+++ src/GNN_Explainability/entrypoints/train_gcn/ba2motifs/kar/pgexplainer.py
@@ -0,0 +1,32 @@
+from tqdm import tqdm
+
+from dig.xgraph.models import *
+import numpy as np
+import torch
+from torch.nn import functional as F
+from torch.optim import Adam
+from torch_geometric.data import DataLoader, Batch
+
+
+from .....config import ROARConfig, TrainingConfig
+from .....enums import *
+from ....core import ROAREntrypoint
+
+class Entrypoint(ROAREntrypoint):
+    
+    def __init__(self):
+        conf = ROARConfig(
+            try_num=17,
+            try_name='kar_pgexplainer_gin3l',
+            dataset_name=Dataset.BA2Motifs,
+            training_config=TrainingConfig(100, OptimType.ADAM),
+            device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu'),
+            save_log_in_file=True,
+            edge_masks_load_dir='../data/ba_2motifs/explanation/pgexplainer',
+            eliminate_top_most_edges=False,
+            roar_ratios=[0.1, 0.3, 0.5, 0.7, 0.9],
+        )
+
+        model = GIN_3l(model_level='graph', dim_node=10, dim_hidden=300, num_classes=2)
+
+        super(Entrypoint, self).__init__(conf, model)
