86c7a5540dcf572c98743e4e94b4f3b9ddcd44b7
AlirezaDizaji <alirezadizaji@yahoo.com>
2022-09-11 13:16:49 +0430

    roar

--- src/GNN_Explainability/utils/roar_visualization.py
+++ src/GNN_Explainability/utils/roar_visualization.py
@@ -45,7 +45,36 @@ if __name__ == "__main__":
     # subgraphx = [100, 80, 80, 80, 75, 65, 65]
     # pgexplainer = [100, 80, 85, 75, 75, 75, 65]
 
-    plt.title('ROAR performance on MUTAG-GIN3l (not applied during evaluation)')
+    # REDDIT-BINARY GCN3l
+    # gnnexplainer = [95, 90.5, 87, 86.5, 80, 78.5, 69]
+    # gradcam = [95, 93, 92.5, 94.5, 92.5, 93.5, 69]
+    # random = [95, 93.25, 91.25, 87, 84.5, 73.75, 69]
+    # # subgraphx = [95, 65, 70, 75, 75, 80, 69]
+    # pgexplainer = [95, 92, 90.5, 88.5, 80.5, 84, 69]
+
+    # REDDIT-BINARY GCN3l (skipped during evaluation)
+    # gnnexplainer = [95, 89.5, 86, 66.5, 54.5, 50, 69]
+    # gradcam = [95, 91, 81.5, 69.5, 72, 68, 69]
+    # random = [95, 93.25, 91.25, 87, 84.5, 73.75, 69]
+    # # subgraphx = [95, 65, 70, 75, 75, 80, 69]
+    # pgexplainer = [95, 90.5, 83.5, 84.5, 77, 71, 69]
+    
+
+    # REDDIT-BINARY GIN3l
+    gnnexplainer = [90.5, 89.5, 91.5, 85, 82, 80.5, 50]
+    gradcam = [90.5, 88, 92, 91.5, 89, 78, 50]
+    random = [90.5, 89.7, 92.5, 89.6, 87, 83.5, 50]
+    # subgraphx = [90.5, 65, 70, 75, 75, 80, 50]
+    pgexplainer = [90.5, 84.5, 76, 71, 79, 66.5, 50]
+
+    # REDDIT-BINARY GIN3l (skipped during evaluation)
+    # gnnexplainer = [90.5, 72, 85.5, 83, 51, 50, 50]
+    # gradcam = [90.5, 70.5, 72, 70.5, 67.5, 74, 50]
+    # random = [90.5, 83.5, 78.75, 69.5, 61.5, 55.75, 50]
+    # # subgraphx = [90.5, 65, 70, 75, 75, 80, 50]
+    # pgexplainer = [90.5, 72, 67.5, 60.5, 52.5, 51, 50]
+
+    plt.title('ROAR performance on REDDIT-GIN3l')
     plt.xlabel("Edge Keep (ROAR %)")
     plt.ylabel("Val Acc (%)")
     plt.plot(x, gnnexplainer)
@@ -57,12 +86,12 @@ if __name__ == "__main__":
     plt.plot(x, random)
     plt.scatter(x, random)
 
-    plt.plot(x, subgraphx)
-    plt.scatter(x, subgraphx)
+    # plt.plot(x, subgraphx)
+    # plt.scatter(x, subgraphx)
     
     plt.plot(x, pgexplainer)
     plt.scatter(x, pgexplainer)
 
-    plt.legend(['GNNExplainer', 'GradCAM', 'Random', 'SubgraphX', 'PGExplainer'])
-    # plt.legend(['GNNExplainer', 'GradCAM', 'Random', 'PGExplainer'])
+    # plt.legend(['GNNExplainer', 'GradCAM', 'Random', 'SubgraphX', 'PGExplainer'])
+    plt.legend(['GNNExplainer', 'GradCAM', 'Random', 'PGExplainer'])
     plt.show()
