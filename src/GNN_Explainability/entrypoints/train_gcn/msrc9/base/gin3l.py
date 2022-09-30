bf962abaa33e598369f9e67d8f06251904e37e70
AlirezaDizaji <alirezadizaji@yahoo.com>
2022-09-30 13:46:58 +0330

    Fix again

--- src/GNN_Explainability/entrypoints/train_gcn/msrc9/base/gin3l.py
+++ src/GNN_Explainability/entrypoints/train_gcn/msrc9/base/gin3l.py
@@ -14,7 +14,7 @@ class Entrypoint(TrainEntrypoint):
             dataset_name=Dataset.MSRC9,
             training_config=TrainingConfig(500, OptimType.ADAM, early_stop=100),
             device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu'),
-            save_log_in_file=False,
+            save_log_in_file=True,
         )
         
         model = GIN_3l(model_level='graph', dim_node=10, dim_hidden=300, num_classes=8)
